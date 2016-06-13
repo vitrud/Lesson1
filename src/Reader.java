ca92d8756a1e99826a30e686da779d0e2e300b61
vitrud <vitrud@ya.ru>
2016-06-13 11:36:57 +0200

    4

--- src/Reader.java
+++ src/Reader.java
@@ -5,14 +5,14 @@ public class Reader {
     String n1, n2;
     
     public void Scan(){
-    	n1 = JOptionPane.showInputDialog("Введите первое001 число");
+    	n1 = JOptionPane.showInputDialog("Введите первое число");
     	n2 = JOptionPane.showInputDialog("Введите второе число");
     	i = Integer.parseInt(n1);
     	k = Integer.parseInt(n2);
     }
     
     public void Scan(int a, int b){
-    	JOptionPane.showMessageDialog(null, "Òåïåðü âàøå ïåðâîå ÷èñëî = "+ a);
-    	JOptionPane.showMessageDialog(null, "Òåïåðü âàøå âòîðîå ÷èñëî = "+ b);
+    	JOptionPane.showMessageDialog(null, "Теперь первое число = "+ a);
+    	JOptionPane.showMessageDialog(null, "Теперь второе число = "+ b);
     }
 }
