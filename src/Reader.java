b7bbe9233e8dcb0cf4f0a6f5082d8202fe5c0b06
vitrud <vitrud@ya.ru>
2016-06-14 16:21:22 +0200

    2

--- src/Reader.java
+++ src/Reader.java
@@ -1,18 +1,62 @@
-import javax.swing.JOptionPane;
 
-public class Reader {
-    int i, k;
-    String n1, n2;
-    
-    public void Scan(){
-    	n1 = JOptionPane.showInputDialog("Введите первое число");
-    	n2 = JOptionPane.showInputDialog("Введите второе число");
-    	i = Integer.parseInt(n1);
-    	k = Integer.parseInt(n2);
-    }
-    
-    public void Scan(int a, int b){
-    	JOptionPane.showMessageDialog(null, "Теперь первое число = "+ a);
-    	JOptionPane.showMessageDialog(null, "Теперь второе число = "+ b);
-    }
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Reader extends JFrame{
+	JButton b1, b2;
+	JLabel l1, l2, l3, l4;
+	JTextField t1, t2;
+	int i, k;
+	String a, b;
+	eHandler handler = new eHandler();
+	
+	public Reader(String s){
+		super(s);
+		setLayout(new FlowLayout());
+		b1 = new JButton("Очистить");
+		b2 = new JButton("Посчитать");
+		l1 = new JLabel("введите первое число");
+		l2 = new JLabel("Введите второе число");
+		l3 = new JLabel("");
+		l4 = new JLabel("");
+		t1 = new JTextField(10);
+		t2 = new JTextField(10);
+		
+		add(b1);
+		add(b2);
+		add(l1);
+		add(t1);
+		add(l2);
+		add(t2);
+		add(l3);
+		add(l4);
+		b2.addActionListener(handler);
+		b1.addActionListener(handler);
+		
+	}
+	
+	public class eHandler implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			try{
+			if(e.getSource()== b2){
+				i = Integer.parseInt(t1.getText());
+				k = Integer.parseInt(t2.getText());
+				i++;
+				k++;
+				a = "Теперь первое число =" + i;
+				b = "Теперь второе число =" + k;
+				l3.setText(a);
+				l4.setText(b);
+			}
+			if(e.getSource() == b1){
+				
+				t1.setText(null);
+				t2.setText(null);
+				l3.setText(null);
+				l4.setText(null);
+			}
+		}catch (Exception ex){JOptionPane.showMessageDialog(null, "Введите число");}
+	}
+  }
 }
